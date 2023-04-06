b7028a4bf17369700f7b2b6abf8b352d1015351f
Alejandro <AleexSolis@hotmail.com>
2023-04-06 09:21:04 -0600

    FUNC (dashboard) Add Navigator

--- App.tsx
+++ App.tsx
@@ -1,20 +1,28 @@
 import { FC } from 'react';
-import { SafeAreaView } from 'react-native';
-import { QueryClient, QueryClientProvider, focusManager } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
 import { Dashboard } from './src/views';
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { retry: 2 } },
 });
 
+const Stack = createNativeStackNavigator();
+
 interface AppProps {}
 
 const App: FC<AppProps> = () => {
   return (
     <QueryClientProvider client={queryClient}>
-      <SafeAreaView style={{ flex: 1 }}>
-        <Dashboard />
-      </SafeAreaView>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Dashboard"
+            component={Dashboard}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
     </QueryClientProvider>
   );
 };
